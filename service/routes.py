a55dfb51de74afc73b84898cd6b3b0ec57a17e36
ivsparrow <ivorobiyenko@gmail.com>
2023-04-26 13:00:21 +0300

    Update routes.py

--- service/routes.py
+++ service/routes.py
@@ -103,7 +103,7 @@ def update_accounts(account_id):
     account.deserialize(request.get_json())
     account.update()
 
-        return account.serialize(), status.HTTP_200_OK
+    return account.serialize(), status.HTTP_200_OK
 
 ######################################################################
 # DELETE AN ACCOUNT
