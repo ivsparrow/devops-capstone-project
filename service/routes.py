6945769e8f4b5180e92749deb0a12a32c0d56484
ivsparrow <ivorobiyenko@gmail.com>
2023-04-26 16:48:08 +0300

    Update routes.py

--- service/routes.py
+++ service/routes.py
@@ -78,6 +78,8 @@ def list_accounts():
 ######################################################################
 # READ AN ACCOUNT
 ######################################################################
+
+
 @app.route("/accounts/<int:account_id>", methods=["GET"])
 def get_accounts(account_id):
     """
@@ -128,7 +130,7 @@ def delete_accounts(account_id):
     app.logger.info("Request to delete an Account with id: %s", account_id)
     account = Account.find(account_id)
     if account:
-            account.delete()
+           account.delete()
     return "", status.HTTP_204_NO_CONTENT
 
 
