11938f3c828495c594d334a2da5f46e3f75756bd
ivsparrow <ivorobiyenko@gmail.com>
2023-04-26 12:58:23 +0300

    Update routes.py

--- service/routes.py
+++ service/routes.py
@@ -100,8 +100,8 @@ def update_accounts(account_id):
     if not account:
             abort(status.HTTP_404_NOT_FOUND, f"Account with id [{account_id}] could not be found.")
 
-        account.deserialize(request.get_json())
-        account.update()
+    account.deserialize(request.get_json())
+    account.update()
 
         return account.serialize(), status.HTTP_200_OK
 
